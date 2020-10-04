01083a8129ba3661d927ac270e8d660dbc6ca69a
li.jon.seow.dev@gmail.com <li.jon.seow.dev@gmail.com>
2020-10-04 16:32:51 +0800

    routing frame

--- src/Component/App.tsx
+++ src/Component/App.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+import StreamCreate from './Streams/StreamCreate';
+import StreamDelete from './Streams/StreamDelete';
+import StreamEdit from './Streams/StreamEdit';
+import StreamList from './Streams/StreamList';
+import StreamShow from './Streams/StreamShow';
+
+import HeaderBanner from './Header/HeaderBanner';
+
 
 const App = (prop : any) =>{
     return (
-        <div> App </div>
+        <div>
+            <BrowserRouter>
+                <div>
+                    <HeaderBanner />
+                    <Route path='/' exact component={StreamList} ></Route>
+                    <Route path='/new'exact component={StreamCreate} ></Route>
+                    <Route path='/edit' exact component={StreamEdit} ></Route>
+                    <Route path='/delete' exact component={StreamDelete} ></Route>
+                    <Route path='/show' exact component={StreamShow} ></Route>
+                </div>
+            </BrowserRouter>
+        </div>
+
     )
 }
 
---
+++ src/Component/Streams/StreamDelete.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+const StreamDelete = () => {
+    return(
+        <div> StreamDelete </div>
+    )
+}
+
+export default StreamDelete;

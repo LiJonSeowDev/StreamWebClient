419e7d1fe71db8f6fd9fbc26b73b0863c9cb1d28
li.jon.seow.dev@gmail.com <li.jon.seow.dev@gmail.com>
2020-10-07 19:17:22 +0800

    more redux boilerplates

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
 
 
 import App from './Component/App';
+import allReducers from './Reducers';
+
+const store = createStore(allReducers);
 
 ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
 
-    <App />,
+    ,
     document.querySelector('#root')
 );
 
